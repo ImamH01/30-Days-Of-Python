d674496e6864551793ae5c4aa9537dd707d5b9d2
ImamH01 <imamhaq01@gmail.com>
2023-03-20 00:37:15 +0000

    commit

---
+++ 08_Day_Dictionaries/Exercise_8.py
@@ -0,0 +1,31 @@
+# 1
+dog = {}
+
+# 2
+dog["name"] = "Buddy"
+dog["age"] = 9
+dog["breed"] = "Labrador"
+dog["legs"] = 4
+print(dog)
+
+# 3
+std_dict = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "age": 27,
+    "marital_status": "single",
+    "skills": ["Python", "JavaScript", "ReactJS"],
+    "country": "Finland",
+    "city": "Helsinki",
+    "address": "bethnal}",
+}
+print(std_dict)
+
+# 4
+print(len(std_dict))
+
+# 5
+skill = std_dict["skills"]
+print(type(skill))
+
+# 6
